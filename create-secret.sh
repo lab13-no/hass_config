45e7630cd517c271dc724d0c6b745036c488e35d
Flemming Richter <quatrox@member.fsf.org>
2017-09-10 01:53:20 +0200

    minor fixes for create-secret.sh

--- create-secret.sh
+++ create-secret.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-SECRETS=$(cat configuration.yaml | grep -o "\!secret .*" | awk '{print $2}')
+
+set -e
+cd `dirname $0`
+
+typeset -r SECRETS=$(grep -o \!secret\ .* configuration.yaml |awk '{print $2}')
 for item in $SECRETS
 do
 	echo $item;
 	read input
 	echo "$item: $input" >> secrets.yaml
 done
-
 
